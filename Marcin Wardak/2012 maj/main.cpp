dbc1e304c21a29cc023dcac985a84c21e00dcdf0
Wardak <marcin.wardak@zagle.net>
2022-02-24 16:00:52 +0100

    matura 2012 maj

---
+++ Marcin Wardak/2012 maj/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+string szyfr (string slowo, string kod)
+{
+	for (short x = 0; x < slowo.length(); ++x)
+	{
+		
+	}
+}
+
+int main(int argc, char** argv)
+{
+	string slowo, kod, szyfr;
+	while (true)
+	{
+		
+	}
+	return 0;
+}
